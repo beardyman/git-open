fc2dfa490591051e383c64db65bda0b768e99ad3
Beardyman <therealbeardyman@gmail.com>
2020-06-26 17:14:52 -0400

    added code to handle clones from ssh

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # git-open
 
 Opens the remote UI of your git repository
+
+## Installation
+```shell script
+npm i git-open
+```
+
+## Usage
+```shell script
+git open
+```
+
+## Docs
+```shell script
+man git-open
+```
--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const git = require('simple-git/promise')();
 
 async function getURL() {
   const remotes = await git.getRemotes(true);
-  return _.get(_.find(remotes, {name: 'origin'}), 'refs.fetch');
+  let remoteUrl = _.get(_.find(remotes, {name: 'origin'}), 'refs.fetch');
+
+  // handle ssh clones
+  if (_.startsWith(remoteUrl, 'git@')) {
+    remoteUrl = _.replace(remoteUrl, ':', '/'); // replaces `:` before username
+    remoteUrl = _.replace(remoteUrl, 'git@', 'https://');
+  }
+
+  return remoteUrl;
 }
 
 
-async function main(args) {
+async function main() {
   const url = await getURL();
   if (url) {
     console.log(`Opening ${url}`);
